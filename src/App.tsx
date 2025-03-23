eba747906da2d1431084b47a3b9770223b38ba70
Carlos Eduardo <cadumielkezim@gmail.com>
2025-03-23 00:01:39 -0300

    ✨ Cria primeiras rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import AppBar from './components/AppBar';
-import CardMain from './components/CardMain';
 
 function App() {
 	return (
 		<div className="bg-white dark:bg-[#00205E] p-0 h-screen m-0">
 			<AppBar />
 			<div className="mt-10 px-3">
-				<CardMain />
+				<Outlet />
 			</div>
 		</div>
 	);
