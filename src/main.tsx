ab1774aa072d74c63a9868c4d77415ec153eb32b
Carlos Eduardo <cadumielkezim@gmail.com>
2025-03-23 22:21:06 -0300

    ✨ Criada tabela de mensalidades

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import './i18n';
 import CardMain from '@/components/CardMain.tsx';
-import MonthlyFees from '@/pages/MonthlyFees.tsx';
+import MonthlyFees from '@/pages/mensalidades/MonthlyFees.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
